a65cea7f094c0ac3b4aef430681b4fc1f5f964e8
root <root@nerthvirt.nerth.net>
2015-08-18 00:54:59 -0400

    added php script to verify mysql connection is successful, updated readme with details

---
+++ info.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "dbuser";
+$password = "dbpassword";
+
+// create connection
+$conn = new mysqli($servername, $username, $password);
+
+// check connection
+if ($conn->connect_error){
+	die("connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$conn->close();
+?>
